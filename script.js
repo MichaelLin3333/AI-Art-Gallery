49c2236b7664cbef3586537fe0804433bedc685d
Michael Lin <lin1164174@gmail.com>
2025-03-31 00:04:12 +0800

    Updated file

--- script.js
+++ script.js
@@ -11,4 +11,13 @@ var swiper = new Swiper(`[unique-script-id="w-w-dm-id"] .mySwiper`, {
     nextEl: `[unique-script-id="w-w-dm-id"] .swiper-button-nexts`,
     prevEl: `[unique-script-id="w-w-dm-id"] .swiper-button-prevs`,
   },
+  breakpoints: {
+    // Adjust layout based on screen size
+    768: {
+      slidesPerView: 2, // Show two slides on screens larger than 768px
+    },
+    1024: {
+      slidesPerView: 3, // Show three slides on screens larger than 1024px
+    },
+  },
 });
